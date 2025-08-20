e1a6eeb1451c0b6c9e09633431426efc06bf9e77
AnvarRocket888 <anvarsalakhov@syndicate-g.com>
2025-08-20 17:13:55 +0300

    att

--- Assets/_gameAssets/Scripts/ScreenManager.cs
+++ Assets/_gameAssets/Scripts/ScreenManager.cs
@@ -2,10 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Unity.Advertisement.IosSupport;
 public class ScreenManager : MonoBehaviour
 {
     public GameObject Menu, Game, Result, allQuestions;
     public string privacyLink;
+    void Start()
+    {
+
+        StartCoroutine(delayActions());
+        IEnumerator delayActions()
+        {
+            yield return new WaitForSeconds(0.5f);
+            if (ATTrackingStatusBinding.GetAuthorizationTrackingStatus() == ATTrackingStatusBinding.AuthorizationTrackingStatus.NOT_DETERMINED)
+            {
+                ATTrackingStatusBinding.RequestAuthorizationTracking();
+            }
+        }
+    }
     public void CloseAll()
     {
         Menu.SetActive(false);
